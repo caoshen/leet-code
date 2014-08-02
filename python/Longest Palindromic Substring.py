a4674d11b486f0a6151674ee55a5047974d1a760
caoshen <ceeseemail@gmail.com>
2014-08-02 22:17:16 +0800

    first commit

--- python/Longest Palindromic Substring.py
+++ python/Longest Palindromic Substring.py
@@ -1,4 +1,22 @@
 class Solution:
-    # @return a boolean
-    def isPalindrome(self, x):
-        return str(x) == str(x)[::-1]
+    # @return a string
+    def longestPalindrome(self, s):
+        str = '^#' + '#'.join(s) + '#$'
+        mx, id = 0, 0
+        p = [0] * len(str)
+        for i in range(1, len(str) - 1):
+            if mx > i:
+                p[i] = min(p[2 * id - i], mx - i)
+            else:
+                p[i] = 1
+            while  str[i - p[i]] == str[i + p[i]]:
+                p[i] += 1
+            if mx < i + p[i]:
+                mx = i + p[i]
+                id = i
+        maxlen = max(p[1 : len(str) - 1]);
+        pos = p.index(maxlen)
+        start = (pos  - maxlen) / 2
+        end = (pos + maxlen) / 2 - 1
+        return s[start: end]
+        
